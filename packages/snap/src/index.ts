7fcaf2b0cf9cca3942426499c9e0001ef2b631e1
Alexey Krasnoperov <Alexey.Krasnoperov@gmail.com>
2023-11-02 14:09:31 +0100

    rewrite manual calls to contract abi calls, support missing information

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,44 +1,41 @@
-import { AbiCoder, BytesLike } from 'ethers';
+import { ethers, Contract } from 'ethers';
 import { OnTransactionHandler } from '@metamask/snaps-types';
-import { heading, panel, text } from '@metamask/snaps-ui';
+import { heading, panel, text, divider } from '@metamask/snaps-ui';
 
 // Handle outgoing transactions.
-export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
-  const companyInformation = await ethereum.request({
-    method: 'eth_call',
-    params: [
-      {
-        from: transaction.from,
-        to: "0xb439B71fB05BC32325C542246De7563054820Ad4",
-        // Hardcoded Address 0x876182C9669E48CEff1F942C7acB5bB98Ea79753
-        data: "0xebb00796000000000000000000000000876182c9669e48ceff1f942c7acb5bb98ea79753"
-      },
-      "latest"
-    ]
-  });
+export const onTransaction: OnTransactionHandler = async ({ transactionOrigin, transaction }) => {
+  const abi = [
+    "function getAddressInfo(address _address) view returns (string memory, string memory, string memory, string memory)",
+  ]
+  const provider = new ethers.BrowserProvider(ethereum);
+  const contract = new Contract("0xb439B71fB05BC32325C542246De7563054820Ad4", abi, provider);
+  const recipientInformation = await contract.getAddressInfo(transaction.to);
 
-  const decodedCompanyInformation = AbiCoder.defaultAbiCoder().decode(
-    ['string', 'string', 'string', 'string'],
-    companyInformation as BytesLike
-  );
+  const companyName = recipientInformation[0];
+  const lei = recipientInformation[1];
+  const email = recipientInformation[2];
+  const message = recipientInformation[3];
 
-  const companyName = decodedCompanyInformation[0];
-  const lei = decodedCompanyInformation[1];
-  const email = decodedCompanyInformation[2];
-  const message = decodedCompanyInformation[3];
-
+  const panelContent = [];
+  if (!companyName && !lei && !email && !message) {
+    panelContent.push(text("⛔️ No information found for this address ⛔️"));
+  } else {
+    panelContent.push(text("⚠️ Information provided by the address owner was not verified. Make sure you trust this address"));
+    panelContent.push(divider());
+    if (companyName) {
+      panelContent.push(heading(companyName));
+    }
+    if (lei) {
+      panelContent.push(text(`LEI: **${lei}**`));
+    }
+    if (email) {
+      panelContent.push(text(`Email: **${email}**`));
+    }
+    if (message) {
+      panelContent.push(text(`Message: **${message}**`));
+    }
+  }
   return {
-    content: panel([
-      heading(companyName),
-      text(
-        `LEI: ${lei}`,
-      ),
-      text(
-        `Email: ${email}`,
-      ),
-      text(
-        `Message: ${message}`,
-      )
-    ]),
+    content: panel(panelContent),
   };
 };
