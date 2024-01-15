4db5f23e9355acd521419fbbb3d49f50ed161b1f
Alexey Krasnoperov <Alexey.Krasnoperov@gmail.com>
2024-01-15 18:08:51 +0100

    support verified addresses, update the contract address

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -5,22 +5,27 @@ import { heading, panel, text, divider } from '@metamask/snaps-ui';
 // Handle outgoing transactions.
 export const onTransaction: OnTransactionHandler = async ({ transactionOrigin, transaction }) => {
   const abi = [
-    "function getAddressInfo(address _address) view returns (string memory, string memory, string memory, string memory)",
+    "function getAddressInfo(address _address) view returns (string memory, string memory, string memory, string memory, bool)",
   ]
   const provider = new ethers.BrowserProvider(ethereum);
-  const contract = new Contract("0xb439B71fB05BC32325C542246De7563054820Ad4", abi, provider);
+  const contract = new Contract("0xb13C133A5D20276E18963545075499e191c6E170", abi, provider);
   const recipientInformation = await contract.getAddressInfo(transaction.to);
 
   const companyName = recipientInformation[0];
   const lei = recipientInformation[1];
   const email = recipientInformation[2];
   const message = recipientInformation[3];
+  const isVerified = recipientInformation[4];
 
   const panelContent = [];
   if (!companyName && !lei && !email && !message) {
     panelContent.push(text("⛔️ No information found for this address ⛔️"));
   } else {
-    panelContent.push(text("⚠️ Information provided by the address owner was not verified. Make sure you trust this address."));
+    if (isVerified) {
+      panelContent.push(text("✅ Verified address ✅"));
+    } else {
+      panelContent.push(text("⚠️ Information provided by the address owner was not verified. Make sure you trust this address."));
+    }
     panelContent.push(divider());
     if (companyName) {
       panelContent.push(heading(companyName));
