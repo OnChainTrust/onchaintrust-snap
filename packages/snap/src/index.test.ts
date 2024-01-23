043c39874b3ed4b51cce2d47011357450e086965
Alexey Krasnoperov <Alexey.Krasnoperov@gmail.com>
2024-01-23 01:43:00 +0100

    cleanup tests

--- packages/snap/src/index.test.ts
+++ packages/snap/src/index.test.ts
@@ -1,30 +1,10 @@
 import { installSnap } from '@metamask/snaps-jest';
-// import { onTransaction } from './index';
-// import { expect } from '@jest/globals';
 import { panel, text } from '@metamask/snaps-ui';
 
 describe('onTransaction handler tests', () => {
-  //   beforeAll(async () => {
-  //     if (typeof global.window === 'undefined') {
-  //       global.window = global as any;
-  //     }
-  //   });
-
-  // afterEach(() => {
-  //     // Reset mock after each test
-  //     jest.resetAllMocks();
-  // });
-
-  // afterAll(() => {
-  //     // Cleanup: remove the mock from window
-  //     delete global.window.ethereum;
-  // });
-
   it('should handle outgoing transactions', async () => {
     const { sendTransaction } = await installSnap();
 
-    // Call the onTransaction handler
-    // const response = await sendTransaction({});
     const response = await sendTransaction({
       value: '0x0',
       data: '0x',
@@ -34,10 +14,6 @@ describe('onTransaction handler tests', () => {
       nonce: '0x0',
     });
 
-    // Add your assertions here
-    // For example, you can use expect statements to verify the behavior of the function
-    // expect(...).toBe(...);
-    expect(response).toRender(panel([text('Hello, world!')]));
-    // expect(response).toEqual({});
+    expect(response).toRender(panel([text("⛔️ No information found for this address ⛔️")]));
   });
 });
