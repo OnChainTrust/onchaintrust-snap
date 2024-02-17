1ee60253a23fb2fa13b88e5023b93d2654239e14
Alexey Krasnoperov <Alexey.Krasnoperov@gmail.com>
2024-02-17 23:54:00 +0100

    lint:fix

--- packages/onchaintrust-snap/src/index.ts
+++ packages/onchaintrust-snap/src/index.ts
@@ -28,36 +28,37 @@ export const onTransaction: OnTransactionHandler = async ({
     })
     .catch((err) => console.error(err));
 
-  const uiElements: UiElement[] = uiDefinition.reduce((acc: UiElement[], element: ElementDefinition) => {
-    switch (element.type) {
-      case 'heading':
-        if (element.value) {
-          acc.push(heading(element.value));
-        }
-        break;
-      case 'text':
-        if (element.value) {
-          acc.push(text(element.value));
-        }
-        break;
-      case 'divider':
-        acc.push(divider());
-        break;
-      case 'copyable':
-        if (element.value) {
-          acc.push(copyable(element.value));
-        }
-        break;
-      case 'image':
-        if (element.value) {
-          acc.push(image(element.value || ''));
-        }
-        break;
-      default:
-        break;
-    }
-    return acc;
-  },
+  const uiElements: UiElement[] = uiDefinition.reduce(
+    (acc: UiElement[], element: ElementDefinition) => {
+      switch (element.type) {
+        case 'heading':
+          if (element.value) {
+            acc.push(heading(element.value));
+          }
+          break;
+        case 'text':
+          if (element.value) {
+            acc.push(text(element.value));
+          }
+          break;
+        case 'divider':
+          acc.push(divider());
+          break;
+        case 'copyable':
+          if (element.value) {
+            acc.push(copyable(element.value));
+          }
+          break;
+        case 'image':
+          if (element.value) {
+            acc.push(image(element.value || ''));
+          }
+          break;
+        default:
+          break;
+      }
+      return acc;
+    },
     [],
   );
 
--- packages/onchaintrust-snap/src/ui-elements.ts
+++ packages/onchaintrust-snap/src/ui-elements.ts
@@ -1,29 +1,34 @@
-import { NodeType } from "@metamask/snaps-sdk";
+import { NodeType } from '@metamask/snaps-sdk';
 
-interface HeadingElement {
-    type: NodeType.Heading;
-    value: string;
-}
+type HeadingElement = {
+  type: NodeType.Heading;
+  value: string;
+};
 
-interface TextElement {
-    type: NodeType.Text;
-    value: string;
-    markdown?: boolean | undefined;
-}
+type TextElement = {
+  type: NodeType.Text;
+  value: string;
+  markdown?: boolean | undefined;
+};
 
-interface DividerElement {
-    type: NodeType.Divider;
-}
+type DividerElement = {
+  type: NodeType.Divider;
+};
 
-interface CopyableElement {
-    type: NodeType.Copyable;
-    value: string;
-    sensitive?: boolean | undefined;
-}
+type CopyableElement = {
+  type: NodeType.Copyable;
+  value: string;
+  sensitive?: boolean | undefined;
+};
 
-interface ImageElement {
-    type: NodeType.Image;
-    value: string;
-}
+type ImageElement = {
+  type: NodeType.Image;
+  value: string;
+};
 
-export type UiElement = HeadingElement | TextElement | DividerElement | CopyableElement | ImageElement;
+export type UiElement =
+  | HeadingElement
+  | TextElement
+  | DividerElement
+  | CopyableElement
+  | ImageElement;
