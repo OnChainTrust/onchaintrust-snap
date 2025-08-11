bacb9d9a1111eb0cb60c62e21374bdd74c5e41ea
Alexey Krasnoperov <Alexey.Krasnoperov@gmail.com>
2025-08-11 02:34:45 +0200

    update few more packages, copy parts of the code from newer snap

--- packages/onchaintrust-snap/snap.config.ts
+++ packages/onchaintrust-snap/snap.config.ts
@@ -2,7 +2,6 @@ import { resolve } from 'path';
 import type { SnapConfig } from '@metamask/snaps-cli';
 
 const config: SnapConfig = {
-  bundler: 'webpack',
   input: resolve(__dirname, 'src/index.ts'),
   server: {
     port: 8080,
